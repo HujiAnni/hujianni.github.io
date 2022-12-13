06fb99aceb4178df9b0c31cb881cd33f2680ac80
Jianni Hu <hujianni.art@qq.com>
2022-12-12 20:47:01 -0500

    adjust image and starting js html manipulation

---
+++ script.js
@@ -0,0 +1,20 @@
+const skills = {
+    businessSkill: {
+        name: ["CRM & CDP", "Digital Marketing"],
+        desc: ["Manage multiple projects on omnichannel clienteling and multimedia customer journey building covering acquisition, retention, and reactivation.",
+            "Execute creative content and digital strategy on multiple digital channels, paid and generic, to drive relevant traffic and conversion."],
+        img_src: ["https://media0.giphy.com/media/BoC8P5mobclKtxGgPk/200.webp?cid=ecf05e47go9n40aox0w9skrnym9e1yiup4gbvticcoz8gucf&rid=200.webp&ct=s",
+                "https://media2.giphy.com/media/gxr0SeRNwNr4wZpEz5/200w.webp?cid=ecf05e47nffiil12odaab5elachskol3gq4msk9svwbbujgw&rid=200w.webp&ct=s"]
+    },
+    technicalSkill: {
+        name:["Data Science","Web Development"],
+        desc:["Generate data-driven visual business insights and action plans by exploratory and predictive analysis with SQL, Python, and R, and data visualization using Tableau, PowerBI, and matplotlib/seaborn.",
+            "Build and design front-end and back-end of interactive and engaging web apps with personalized treatment."],
+        img_src:["https://media4.giphy.com/media/SvEUbsayyUlcPm41Tl/200w.webp?cid=ecf05e47iaq4b3jab1tydb1pwh48jz8pxyp1hd8j67rrvr5s&rid=200w.webp&ct=s",
+                "https://media4.giphy.com/media/juua9i2c2fA0AIp2iq/200w.webp?cid=ecf05e47339jebyt5rl5bqs2fpbzublve6xv1pn729rpxxev&rid=200w.webp&ct=s"]
+    }
+}
+
+
+// Add skills to html by DOM
+
